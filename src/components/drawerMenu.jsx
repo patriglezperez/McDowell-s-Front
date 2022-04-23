7b943c7dfc22ea0a5634bf0061c4545865230906
fernandocieri <fernandocieri@gmail.com>
2022-04-23 14:35:57 +0200

    Builds layout of drawerMenu

---
+++ src/components/drawerMenu.jsx
@@ -0,0 +1,136 @@
+import { useState } from "react";
+
+import { styled } from '@mui/material/styles';
+import ArrowForwardIosSharpIcon from '@mui/icons-material/ArrowForwardIosSharp';
+import MuiAccordion from '@mui/material/Accordion';
+import MuiAccordionSummary from '@mui/material/AccordionSummary';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import IconButton from '@mui/material/IconButton';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import MenuIcon from '@mui/icons-material/Menu';
+
+export default function DrawerMenu() {
+    const drawerPosition = 'left';
+    const greenDark = '#256A25';
+
+    const [openDrawer, setOpenDrawer] = useState({
+        [drawerPosition]: false
+    });
+    const [expandAccordion, setExpandAccordion] = useState('panel1');
+
+
+    const Accordion = styled((props) => (
+        <MuiAccordion disableGutters elevation={0} square {...props} />
+    ))(({ theme }) => ({
+        '&:not(:last-child)': {
+            borderBottom: 0,
+        },
+        '&:before': {
+            display: 'none',
+        },
+    }));
+
+    const AccordionSummary = styled((props) => (
+        <MuiAccordionSummary
+            expandIcon={<ArrowForwardIosSharpIcon sx={{ fontSize: '1.4rem', color: `${greenDark}` }} />}
+            {...props}
+        />
+    ))(({ theme }) => ({
+        backgroundColor: '#ffffff',
+        flexDirection: 'row-reverse',
+        '& .MuiAccordionSummary-expandIconWrapper.Mui-expanded': {
+            transform: 'rotate(90deg)',
+        },
+        '& .MuiAccordionSummary-content': {
+            marginLeft: '5px',
+            fontSize: '1.6rem',
+        },
+    }));
+
+    const toggleAccordion = (panel) => (event, newExpanded) => {
+        setExpandAccordion(newExpanded ? panel : false);
+    };
+
+    const toggleDrawer = (anchor, open) => (event) => {
+        if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+            return;
+        }
+        setOpenDrawer({ ...openDrawer, [anchor]: open });
+    };
+
+    const list = (anchor) => (
+        <Box role="presentation">
+         
+            <List>
+                <ListItem button
+                    onClick={toggleDrawer(anchor, false)}
+                    onKeyDown={toggleDrawer(anchor, false)}
+                    sx={{marginLeft:'1.8rem', marginBottom:'1rem', marginTop:'1.5rem'}}
+                >
+                    <p className='drawer-item'>Inicio</p>
+                </ListItem>
+
+                <Accordion expanded={expandAccordion === 'panel1'} onChange={toggleAccordion('panel1')} sx={{marginBottom:'1rem'}}>
+                    <AccordionSummary aria-controls="panel1d-content" id="panel1d-header">
+                        <p className='drawer-item'>
+                            Pedidos
+                        </p>
+                    </AccordionSummary>
+
+                    <ListItem button
+                        sx={{ marginLeft: '2.8rem', marginBottom:'0.5rem' }}
+                        onClick={toggleDrawer(anchor, false)}
+                        onKeyDown={toggleDrawer(anchor, false)}
+                    >
+                        <p className='drawer-item'>
+                            Pedidos en curso
+                        </p>
+                    </ListItem>
+
+                    <ListItem button
+                        sx={{ marginLeft: '2.8rem', marginBottom:'0.5rem' }}
+                        onClick={toggleDrawer(anchor, false)}
+                        onKeyDown={toggleDrawer(anchor, false)}
+                    >
+                        <p className='drawer-item' >
+                            Historial de pedidos
+                        </p>
+                    </ListItem>
+                </Accordion>
+
+                <ListItem button
+                    onClick={toggleDrawer(anchor, false)}
+                    onKeyDown={toggleDrawer(anchor, false)}
+                    sx={{marginLeft:'1.8rem', marginBottom:'1rem'}}
+                >
+                    <p className='drawer-item'>Personal</p>
+                </ListItem>
+
+                <ListItem button
+                    onClick={toggleDrawer(anchor, false)}
+                    onKeyDown={toggleDrawer(anchor, false)}
+                    sx={{marginLeft:'1.8rem', marginBottom:'1rem'}}
+                >
+                    <p className='drawer-item'>Cerrar sesión</p>
+                </ListItem>
+            </List >
+        </Box >
+    );
+
+    return (
+        <>
+            <IconButton onClick={toggleDrawer('left', true)} sx={{ width: 'fit-content' }} >
+                <MenuIcon sx={{ color: '#ffffff', fontSize: '3rem', cursor: 'pointer' }} />
+            </IconButton>
+            <Drawer
+                anchor={drawerPosition}
+                open={openDrawer[drawerPosition]}
+                onClose={toggleDrawer(drawerPosition, false)}
+            >
+                {list(drawerPosition)}
+            </Drawer>
+        </>
+    );
+}

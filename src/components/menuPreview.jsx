3c2c00d4893986f62d6169e40e8cf4cad1c51b30
patriglezperez <patriciaggonzalezperez@hotmail.com>
2022-04-25 20:53:42 +0200

    simplified menuPreview component

--- src/components/menuPreview.jsx
+++ src/components/menuPreview.jsx
@@ -2,26 +2,43 @@ import burguer from "../../src/assets/img/logoBurguer.png";
 import menuBurguer from "../assets/img/Menu1.png";
 import React, { useState } from "react";
 
-function MenuPreview() {
-  const menuMcDowells = {
+const menus = [
+  {
     name: "Menu McDowell's",
     price: "6.95€",
-  };
-
-  const menuJr = {
+    image: burguer,
+  },
+  {
     name: "Menu McDowell's Jr",
     price: "5.99€",
-  };
-
-  const [menu1, setMenu1] = useState(menuMcDowells);
-  const [menu2, setMenu2] = useState(menuJr);
-
-  console.log(menu1, "este es el menu1");
+    image: menuBurguer,
+  },
+];
+function MenuPreview() {
   return (
     <>
-      <div class="card">
+      {menus.map((menu) => (
+        <div class="card">
+          <div class="blob"></div>
+          <img src={menu.image} alt="logoBurguer" className="img" />
+          <h2>
+            {menu.name}
+            <br />
+            <span>{menu.price}</span>
+          </h2>
+          <p>
+            <b>Añadir</b>
+          </p>
+        </div>
+      ))}
+    </>
+  );
+}
+export default MenuPreview;
+
+/*<div class="card">
         <div class="blob"></div>
-        <img src={burguer} alt="logoBurguer" className="img" />
+        <img src={menu1.image.burguer} alt="logoBurguer" className="img" />
         <h2>
           {menu1.name}
           <br />
@@ -33,7 +50,7 @@ function MenuPreview() {
       </div>
       <div class="card">
         <div class="blob"></div>
-        <img src={menuBurguer} alt="logoBurguer" className="img" />
+        <img src={menu2.image.menuBurguer} alt="logoBurguer" className="img" />
         <h2>
           {menu2.name}
           <br />
@@ -43,29 +60,4 @@ function MenuPreview() {
           <b>Añadir</b>
         </p>
       </div>
-    </>
-  );
-}
-export default MenuPreview;
-{
-  /* <div>
-  <div className="menu">
-    <img src={menuBurguer} alt="logoBurguer" className="burguer" />
-    <p className="description">
-      <b>{menu1.name}</b>
-      <br />
-
-      <span>{menu1.price}</span>
-    </p>
-  </div>
-  <div className="menu">
-    <img src={burguer} alt="logoBurguer" className="burguer" />
-    <p className="description">
-      <b>{menu2.name}</b>
-      <br />
-
-      <span>{menu2.price}</span>
-    </p>
-  </div>
-</div>; */
-}
+*/

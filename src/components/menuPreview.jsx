6d44bdbf6d197440247d5c3d209343a8c9cfbeda
patriglezperez <patriciaggonzalezperez@hotmail.com>
2022-04-25 18:50:38 +0200

    test component

--- src/components/menuPreview.jsx
+++ src/components/menuPreview.jsx
@@ -1,22 +1,71 @@
-import logoBurguer from "../../src/assets/img/logoBurguer.png";
+import burguer from "../../src/assets/img/logoBurguer.png";
+import menuBurguer from "../assets/img/Menu1.png";
+import React, { useState } from "react";
 
 function MenuPreview() {
-  return (
-    <div>
-      <div className="menu">
-        <img src={logoBurguer} alt="logoBurguer" className="burguer" />
-        <p className="description">
-          <b>
-            Menu
-            <br />
-            McDowell's
-          </b>
-          <br />
+  const menuMcDowells = {
+    name: "Menu McDowell's",
+    price: "6.95€",
+  };
 
-          <span>6.95€</span>
+  const menuJr = {
+    name: "Menu McDowell's Jr",
+    price: "5.99€",
+  };
+
+  const [menu1, setMenu1] = useState(menuMcDowells);
+  const [menu2, setMenu2] = useState(menuJr);
+
+  console.log(menu1, "este es el menu1");
+  return (
+    <>
+      <div class="card">
+        <div class="blob"></div>
+        <img src={burguer} alt="logoBurguer" className="img" />
+        <h2>
+          {menu1.name}
+          <br />
+          <span>{menu1.price}</span>
+        </h2>
+        <p>
+          <b>Añadir</b>
         </p>
       </div>
-    </div>
+      <div class="card">
+        <div class="blob"></div>
+        <img src={menuBurguer} alt="logoBurguer" className="img" />
+        <h2>
+          {menu2.name}
+          <br />
+          <span>{menu2.price}</span>
+        </h2>
+        <p>
+          <b>Añadir</b>
+        </p>
+      </div>
+    </>
   );
 }
 export default MenuPreview;
+{
+  /* <div>
+  <div className="menu">
+    <img src={menuBurguer} alt="logoBurguer" className="burguer" />
+    <p className="description">
+      <b>{menu1.name}</b>
+      <br />
+
+      <span>{menu1.price}</span>
+    </p>
+  </div>
+  <div className="menu">
+    <img src={burguer} alt="logoBurguer" className="burguer" />
+    <p className="description">
+      <b>{menu2.name}</b>
+      <br />
+
+      <span>{menu2.price}</span>
+    </p>
+  </div>
+</div>; */
+}

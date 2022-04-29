21ae4a597c3a945c9429cbd68b8d58e0375d8934
fernandocieri <fernandocieri@gmail.com>
2022-04-29 20:43:33 +0200

    Creates a services that hosts all Amplify-related functions

---
+++ src/components/services/amplifyService.js
@@ -0,0 +1,109 @@
+import { Auth } from 'aws-amplify';
+
+let user = {};
+const tokenKeyNames = { idToken: 'idToken', refreshToken: 'refreshToken', userId: 'userId' };
+const exceptions = { setNewPassword: 'NEW_PASSWORD_REQUIRED', refreshSession: 'NotAuthorizedException', usernameExists: 'UsernameExistsException', userNotConfirmed: 'UserNotConfirmedException', userNotFound: 'UserNotFoundException' };
+let defaultLocalStorage;
+if (typeof window !== 'undefined') {
+    defaultLocalStorage = { ...localStorage };
+}
+
+function setInLocalStorage(keys, values) {
+    for (let i = 0; i < keys.length; i++) {
+        localStorage.setItem(keys[i], values[i]);
+    }
+}
+
+function removeFromLocalStorage(keys) {
+    for (let i = 0; i < keys.length; i++) {
+        localStorage.removeItem(keys[i]);
+    }
+}
+
+function setDefaultLocalStorage() {
+    localStorage.clear();
+    Object.entries(defaultLocalStorage).forEach(([key, value]) => {
+        localStorage.setItem(key, value);
+    });
+}
+export default class AuthService {
+    static failed = 0;
+    static success = 1;
+    static newPasswordRequired = 2;
+    static sessionExpired = 3;
+    static userExists = 4;
+    static userNotConfirmed = 5;
+    static userNotFound = 6;
+
+    static handleSignUp = async (userInput) => {
+        try {
+            const { user } = await Auth.signUp({
+                username: userInput.email,
+                password: userInput.password,
+                attributes: {
+                    email: userInput.email,
+                }
+            });
+            return this.success;
+        } catch (error) {
+            if (error.name === exceptions.usernameExists) {
+                return this.userExists;
+            } else {
+                return this.failed;
+            }
+        }
+    }
+
+    static handleSignIn = async (userInput) => {
+        try {
+            user = await Auth.signIn(userInput.email, userInput.password);
+            if (user.challengeName === exceptions.setNewPassword) {
+                return this.newPasswordRequired;
+            } else {
+                setDefaultLocalStorage();
+                const keys = [tokenKeyNames.idToken, tokenKeyNames.refreshToken, tokenKeyNames.userId];
+                const values = [user.signInUserSession.idToken.jwtToken, user.signInUserSession.refreshToken.token, user.attributes.sub]
+                setInLocalStorage(keys, values);
+                return this.success;
+            }
+        } catch (error) {
+            if (error.name === exceptions.userNotConfirmed) {
+                return this.userNotConfirmed;
+            } else if (error.name === exceptions.userNotFound) {
+                return this.userNotFound;
+            } else {
+                return this.failed;
+            }
+        }
+    }
+
+    static handleSetPassword = async (userInput) => {
+        try {
+            await Auth.completeNewPassword(
+                user,
+                userInput.newPassword
+            );
+            setDefaultLocalStorage();
+            const keys = [tokenKeyNames.idToken, tokenKeyNames.refreshToken, tokenKeyNames.userId];
+            const values = [user.signInUserSession.idToken.jwtToken, user.signInUserSession.refreshToken.token, user.attributes.sub]
+            setInLocalStorage(keys, values);
+            return this.success;
+        } catch (error) {
+            if (error._type === exceptions.refreshSession) {
+                return this.sessionExpired;
+            }
+            return this.failed;
+        }
+    }
+    
+    static handleSignOut = async () => {
+        try {
+            await Auth.signOut();
+            const keys = [tokenKeyNames.idToken, tokenKeyNames.refreshToken, tokenKeyNames.userId];
+            removeFromLocalStorage(keys);
+            return this.success;
+        } catch (error) {
+            return this.failed;
+        }
+    }
+}

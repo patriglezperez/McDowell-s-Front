342a0a087974047da7ec11842670cba8c5a3bc07
fernandocieri <fernandocieri@gmail.com>
2022-04-30 19:29:14 +0200

    Adds amplify import and configuration

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import Header from "./components/header";
 import StaffSignIn from "./components/StaffSignIn";
 import MenuPreview from "./components/menuPreview";
 
+import Amplify, { Auth } from 'aws-amplify';
+import awsconfig from './aws-exports';
+Amplify.configure(awsconfig);
+
 function App() {
   return (
     <>

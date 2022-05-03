83e4ec66bae6e2e3b78df3efcad3a93a59ef5f25
patriglezperez <patriciaggonzalezperez@hotmail.com>
2022-05-03 18:19:23 +0200

    base of the component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import Header from "./components/header";
 import StaffSignIn from "./components/StaffSignIn/StaffSignIn";
 import MenuPreview from "./components/menuPreview/menuPreview";
+import OrderCreator from "./components/orderCreator/OrderCreator";
 
 function App() {
   return (
     <>
       <Header />
       {/* <StaffSignIn /> */}
-      <MenuPreview />
+      {/* <MenuPreview /> */}
+      <OrderCreator />
     </>
   );
 }

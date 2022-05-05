6307f56f0c4f8089d0c7244c83ee02f3e816557f
fernandocieri <fernandocieri@gmail.com>
2022-05-05 20:09:17 +0200

    Removes unnecessary functions and moves all responses into an object

--- src/components/services/amplifyService.js
+++ src/components/services/amplifyService.js
@@ -2,7 +2,8 @@ import { Auth } from 'aws-amplify';
 
 let user = {};
 const tokenKeyNames = { idToken: 'idToken', refreshToken: 'refreshToken', userId: 'userId' };
-const exceptions = { setNewPassword: 'NEW_PASSWORD_REQUIRED', refreshSession: 'NotAuthorizedException', usernameExists: 'UsernameExistsException', userNotConfirmed: 'UserNotConfirmedException', userNotFound: 'UserNotFoundException' };
+const exceptions = { refreshSession: 'NotAuthorizedException', userExists: 'UsernameExistsException', userNotConfirmed: 'UserNotConfirmedException', userNotFound: 'UserNotFoundException' };
+
 let defaultLocalStorage;
 if (typeof window !== 'undefined') {
     defaultLocalStorage = { ...localStorage };
@@ -28,13 +29,7 @@ function setDefaultLocalStorage() {
 }
 
 export default class AmplifyService {
-    static failed = 0;
-    static success = 1;
-    static newPasswordRequired = 2;
-    static sessionExpired = 3;
-    static userExists = 4;
-    static userNotConfirmed = 5;
-    static userNotFound = 6;
+    static responses = { failed: 0, success: 1, newPasswordRequired: 2, sessionExpired: 3, userExists: 4, userNotConfirmed: 5, userNotFound: 6 };
 
     static signUp = async (userInput) => {
         try {
@@ -45,12 +40,12 @@ export default class AmplifyService {
                     email: userInput.email,
                 }
             });
-            return this.success;
+            return this.responses.success;
         } catch (error) {
-            if (error.name === exceptions.usernameExists) {
-                return this.userExists;
+            if (error.name === exceptions.userExists) {
+                return this.responses.userExists;
             } else {
-                return this.failed;
+                return this.responses.failed;
             }
         }
     }
@@ -58,45 +53,19 @@ export default class AmplifyService {
     static signIn = async (userInput) => {
         try {
             user = await Auth.signIn(userInput.email, userInput.password);
-            if (user.challengeName === exceptions.setNewPassword) {
-                return this.newPasswordRequired;
-            } else {
-                setDefaultLocalStorage();
-                const keys = [tokenKeyNames.idToken, tokenKeyNames.refreshToken, tokenKeyNames.userId];
-                const values = [user.signInUserSession.idToken.jwtToken, user.signInUserSession.refreshToken.token, user.attributes.sub]
-                setInLocalStorage(keys, values);
-                return this.success;
-            }
-        } catch (error) {
-            //remove this depending on final sign up workflow
-            if (error.name === exceptions.userNotConfirmed) {
-                return this.userNotConfirmed;
-            } else if (error.name === exceptions.userNotFound) {
-                return this.userNotFound;
-            } else {
-                return this.failed;
-            }
-        }
-    }
-
-    //This is used when a user is created directly from the cognito console
-    //we'll have to decide the workflow we want, but just in case I'll leave it here for now
-    static completePassword = async (userInput) => {
-        try {
-            await Auth.completeNewPassword(
-                user,
-                userInput.newPassword
-            );
             setDefaultLocalStorage();
             const keys = [tokenKeyNames.idToken, tokenKeyNames.refreshToken, tokenKeyNames.userId];
             const values = [user.signInUserSession.idToken.jwtToken, user.signInUserSession.refreshToken.token, user.attributes.sub]
             setInLocalStorage(keys, values);
-            return this.success;
+            return this.responses.success;
         } catch (error) {
-            if (error._type === exceptions.refreshSession) {
-                return this.sessionExpired;
+            if (error.name === exceptions.userNotConfirmed) {
+                return this.responses.userNotConfirmed;
+            } else if (error.name === exceptions.userNotFound) {
+                return this.responses.userNotFound;
+            } else {
+                return this.responses.failed;
             }
-            return this.failed;
         }
     }
 
@@ -105,9 +74,9 @@ export default class AmplifyService {
             await Auth.signOut();
             const keys = [tokenKeyNames.idToken, tokenKeyNames.refreshToken, tokenKeyNames.userId];
             removeFromLocalStorage(keys);
-            return this.success;
+            return this.responses.success;
         } catch (error) {
-            return this.failed;
+            return this.responses.failed;
         }
     }
 }

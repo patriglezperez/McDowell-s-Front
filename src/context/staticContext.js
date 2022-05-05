801b32cf9344e5e3c3b490ab9cd55281ab89d69a
patriglezperez <patriciaggonzalezperez@hotmail.com>
2022-05-05 12:52:46 +0200

    add menus to the context

--- src/context/staticContext.js
+++ src/context/staticContext.js
@@ -4,8 +4,11 @@ const StaticContext = React.createContext({});
 
 export function StaticContextProvider({ children }) {
   const [uuid_user, setUuid_user] = useState("");
+  const [menus, setMenus] = useState([]);
   return (
-    <StaticContext.Provider value={{ uuid_user, setUuid_user }}>
+    <StaticContext.Provider
+      value={{ uuid_user, setUuid_user, menus, setMenus }}
+    >
       {children}
     </StaticContext.Provider>
   );

7d161077646ae434e355797d82eea3efa90ef436
patriglezperez <patriciaggonzalezperez@hotmail.com>
2022-05-05 17:30:24 +0200

    change name

--- src/context/staticContext.js
+++ src/context/staticContext.js
@@ -3,14 +3,14 @@ import React, { useState } from "react";
 const StaticContext = React.createContext({});
 
 export function StaticContextProvider({ children }) {
-  const [menus, setMenus] = useState([
+  const [order, setOrder] = useState([
     {
       uuid_user: "",
       menus: [],
     },
   ]);
   return (
-    <StaticContext.Provider value={{ menus, setMenus }}>
+    <StaticContext.Provider value={{ order, setOrder }}>
       {children}
     </StaticContext.Provider>
   );

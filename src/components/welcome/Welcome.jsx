256a1ffebeaeeb5ada33d8f0a9e307298a4f5527
patriglezperez <patriciaggonzalezperez@hotmail.com>
2022-05-05 17:21:42 +0200

    I add the variables for how it would be in the context, but I leave them commented for the moment

--- src/components/welcome/Welcome.jsx
+++ src/components/welcome/Welcome.jsx
@@ -3,26 +3,31 @@ import React, { useState, useEffect } from "react";
 import { v4 as uuidv4 } from "uuid";
 import { useNavigate } from "react-router-dom";
 import Header from "../../components/header";
+import { useContext } from "react";
+import StaticContext from "../../context/staticContext";
 
 function Welcome() {
   //   const navigate = useNavigate();
-  const [uuid_user, setUuid_user] = useState("");
-  console.log(uuid_user);
 
-  /*Create a uuuid_user for the new order*/
+  const { uuid_user, setUuid_user } = useContext(StaticContext);
+  // const { menus, setMenus } = useContext(StaticContext);
+
+  //Create a uuuid_user for the new order
   async function start() {
     setUuid_user(uuidv4());
+    // setMenus((menus.uuidUser = uuidv4()));
     console.log("Comenzando pedido");
     // navigate("/");
   }
 
   return (
     <>
+      <Header />
       <div className="start">
         <h1 className="title">McDowell's</h1>
-
+        <br />
         <img src={logoBurguer} alt="logoBurguer" className="logoBurger" />
-
+        <br />
         <div>
           <button type="submit" className="cta" onClick={start}>
             <span>Comenzar pedido</span>

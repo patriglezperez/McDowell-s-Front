613fc6d9afb6ada2d1f12740a70cd16d9da5953b
fernandocieri <fernandocieri@gmail.com>
2022-05-06 19:16:48 +0200

    Adds functionality

--- src/components/confirmOrder/confirmOrder.jsx
+++ src/components/confirmOrder/confirmOrder.jsx
@@ -1,15 +1,47 @@
-import React from 'react'
+import { useContext, useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+import axios from "axios";
+
+import StaticContext from "../../context/staticContext";
 
 export default function ConfirmOrder() {
+    const { order, setOrder } = useContext(StaticContext);
+    const [orderTotal, setOrderTotal] = useState(0);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (order.menus && order.menus.length > 0) {
+            let totalSum = 0;
+            totalSum = order.menus.reduce((previousValue, currentValue) => previousValue + currentValue);
+            setOrderTotal(totalSum);
+        }
+    }, [order])
+
+    function cancelOrder() {
+        setOrder({ id: [], menus: [] });
+        navigate('/customers');
+    }
+
+    function confirmOrder() {
+        const finishedOrder = { order: order.menus };
+        try {
+            const confirmationResponse = axios.post(`${process.env.REACT_APP_API_URL}/orders/new`, { order: finishedOrder });
+            navigate(`customers/order/${order.id[0]}/completed`, { orderNumber: confirmationResponse.data.orderNumber });
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
     return (
         <section className='confirm-order'>
             <p className='section-title'>Resumen de tu pedido</p>
             <span className='price-wrapper'>
                 <p className='total'>TOTAL</p>
-                <b className='total-price'>€12.95</b>
+                <b className='total-price'>€{orderTotal}</b>
             </span>
-            <button className='action-button'>CONFIRMAR PEDIDO</button>
-            <button className='action-button'>CANCELAR PEDIDO</button>
+            <button className='action-button' onClick={confirmOrder}>CONFIRMAR PEDIDO</button>
+            <button className='action-button' onClick={cancelOrder}>CANCELAR PEDIDO</button>
         </section>
     )
 }

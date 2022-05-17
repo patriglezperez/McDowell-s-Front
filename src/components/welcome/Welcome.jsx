afb3561d69507293e40c525144b664b932a5da1c
fernandocieri <fernandocieri@gmail.com>
2022-05-17 19:31:27 +0200

    Remvoevs header from Welcome

--- src/components/welcome/Welcome.jsx
+++ src/components/welcome/Welcome.jsx
@@ -2,7 +2,6 @@ import logoBurguer from "../../assets/img/logoBurguer.png";
 import React, { useState, useEffect } from "react";
 import { v4 as uuidv4 } from "uuid";
 import { useNavigate } from "react-router-dom";
-import Header from "../header";
 import { useContext } from "react";
 import StaticContext from "../../context/staticContext";
 
@@ -19,7 +18,6 @@ function Welcome() {
 
   return (
     <>
-      <Header />
       <div className="start">
         <h1 className="title">McDowell's</h1>
         <br />
